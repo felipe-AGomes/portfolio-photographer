744974695149b95dba3e84da5437e9b123ea5a22
Felipe <falmeidagomes13@gmail.com>
2023-06-04 12:03:14 -0300

    feat: Add prop "personName"

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,8 +1,19 @@
 import { useEffect, useState } from 'react';
 import S from './Header.module.css';
 
-export default function Header() {
+import {} from 'next/font/google';
+import { CategoriesProps } from '@/context/currentPageContext';
+import useCurrentPageContext from '@/hooks/useCurrentPageContext';
+import { useRouter } from 'next/router';
+
+type Props = {
+	personName?: string;
+};
+
+export default function Header({ personName }: Props) {
 	const [scroled, setScroled] = useState<boolean>(false);
+	const { setCurrentPage } = useCurrentPageContext();
+	const router = useRouter();
 
 	useEffect(() => {
 		document.addEventListener('scroll', handleScroll);
@@ -12,14 +23,32 @@ export default function Header() {
 		setScroled(window.scrollY > 0);
 	};
 
+	const handleClick = (category: CategoriesProps | 'contato') => {
+		if (category === 'contato') {
+			router.push('/contato');
+			return;
+		}
+		router.push('/');
+		setCurrentPage(category);
+	};
+
 	return (
-		<header className={`${S.headerContain} ${scroled ? S.scroled : ''}`}>
+		<header
+			className={`${S.headerContain} ${scroled ? S.scroled : ''}
+				${personName ? `${S.personNameContain}` : ''}`}
+		>
+			{personName && (
+				<div>
+					<h1>{personName}</h1>
+				</div>
+			)}
+
 			<ul>
-				<li>Início</li>
-				<li>Retratos</li>
-				<li>Moda/Beleza</li>
-				<li>História</li>
-				<li>Contato</li>
+				<li onClick={() => handleClick('inicio')}>Início</li>
+				<li onClick={() => handleClick('retratos')}>Retratos</li>
+				<li onClick={() => handleClick('moda/beleza')}>Moda/Beleza</li>
+				<li onClick={() => handleClick('historia')}>História</li>
+				<li onClick={() => handleClick('contato')}>Contato</li>
 			</ul>
 		</header>
 	);
